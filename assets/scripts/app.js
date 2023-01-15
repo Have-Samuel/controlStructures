70d16329400baca56ae2f284157b941dc8dd8607
Have Samuel <samiehave9@gmail.com>
2023-01-16 00:35:08 +0300

    Add the monster attack functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const ATTACK_MONSTER_VALUE = 14;
 
 let maxChoosenLife = 100;
 let currentMonsterHealth = maxChoosenLife;
@@ -9,8 +10,12 @@ adjustHealthBars(maxChoosenLife) // Adjust maxLife both Monster and player
 function attackHandler() { // Attack Function
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
+  const playerDamge = dealPlayerDamage(ATTACK_MONSTER_VALUE);
+  currentPlayerHealth -= playerDamge;
   if (currentMonsterHealth <= 0) { // Checking if Monster life in <= 0
     alert('You Won!');
+  } else if (currentPlayerHealth <= 0) {
+    alert('You Lost!');
   }
 }
 
